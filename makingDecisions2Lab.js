c0c54c113b4dbf344a36815642054ec83a35ff93
Toky Chanthavong <tchantha01@gmail.com>
2022-08-21 23:17:32 -0600

    makingDecisions2Lab

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -129,11 +129,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 //Code Here
 
-// for(let i = 0; i < listOfNumbers.length; i++){
-//   if(listOfNumbers[i] % 3 === 0){
-//     console.log(`${listOfNumbers[i]} is divisible by 3`)
-//   }
-// }
+for(let i = 0; i < listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log(`${listOfNumbers[i]} is divisible by 3`)
+  }
+}
 
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
@@ -174,26 +174,26 @@ If the letter grade is not one of the above letters, console.log '.Not an eligib
 
 //Code Here
 
-switch(letterGrade){
-  case 'A':
-    console.log('The student is doing excellently.')
-    break
-  case 'B':
-    console.log('The student is doing well.')
-    break
-  case 'C':
-    console.log('The student is doing alright.') 
-    break
-  case 'D':
-    console.log('The student is not doing very well') 
-    break
-  case 'F':
-    console.log('The student is failing.')  
-    break
-  default:
-    console.log('Not an eligible grade.')    
+// switch(letterGrade){
+//   case 'A':
+//     console.log('The student is doing excellently.')
+//     break
+//   case 'B':
+//     console.log('The student is doing well.')
+//     break
+//   case 'C':
+//     console.log('The student is doing alright.') 
+//     break
+//   case 'D':
+//     console.log('The student is not doing very well') 
+//     break
+//   case 'F':
+//     console.log('The student is failing.')  
+//     break
+//   default:
+//     console.log('Not an eligible grade.')    
 
-}
+// }
 
 
 ////////// PROBLEM 11 //////////
@@ -221,15 +221,15 @@ switch(letterGrade){
 */
 
 //Code Here
- for(let i = 1; i < 100; i++){
-  if(i % 3 === 0 && i % 5 === 0 ){
-    console.log('DevMountain')
-  }else if(i % 3 === 0){
-    console.log('Dev')
-  }else if(i % 5 === 0){
-    console.log('Mountain')
+//  for(let i = 1; i < 100; i++){
+//   if(i % 3 === 0 && i % 5 === 0 ){
+//     console.log('DevMountain')
+//   }else if(i % 3 === 0){
+//     console.log('Dev')
+//   }else if(i % 5 === 0){
+//     console.log('Mountain')
 
-  }else{
-    console.log(i)
-  }
- }
+//   }else{
+//     console.log(i)
+//   }
+//  }
